00b633c5525f97ad89dc93067dce578233494ae7
Tanguy Gigarel <gigarelt@gmail.com>
2017-03-09 14:58:03 +0100

    findAll fonctionnel

--- pizzeria-app/pizzeria-dao/src/main/java/fr/pizzeria/dao/PizzaDaoDataBase.java
+++ pizzeria-app/pizzeria-dao/src/main/java/fr/pizzeria/dao/PizzaDaoDataBase.java
@@ -2,6 +2,7 @@ package fr.pizzeria.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -13,7 +14,6 @@ import fr.pizzeria.model.Pizza;
 
 public class PizzaDaoDataBase implements IDao<Pizza> {
 
-	private Connection conn = null;
 	private String driver;
 	private String dbURL;
 	private String user;
@@ -23,6 +23,7 @@ public class PizzaDaoDataBase implements IDao<Pizza> {
 		// récupération des informations de connexion depuis le fichier
 		// jdbc.properties
 		ResourceBundle bundle = ResourceBundle.getBundle("jdbc");
+
 		driver = bundle.getString("database.driver");
 		dbURL = "jdbc:" + bundle.getString("database.type") + "://" + bundle.getString("database.host") + ":"
 				+ bundle.getString("database.port") + "/" + bundle.getString("database.name");
@@ -32,17 +33,12 @@ public class PizzaDaoDataBase implements IDao<Pizza> {
 
 	@Override
 	public List<Pizza> findAll() throws StockageException {
-		/*
-		 * try { Class.forName(driver); } catch (ClassNotFoundException e1) {
-		 * e1.printStackTrace(); }
-		 */
-		try (Connection conn = DriverManager.getConnection(dbURL, user, password)) {
 
-			String sql = "SELECT * FROM pizza";
+		String sql = "SELECT * FROM pizza";
 
-			Statement statement = conn.createStatement();
-
-			ResultSet result = statement.executeQuery(sql);
+		try (Connection connexion = DriverManager.getConnection(dbURL, user, password);
+				Statement statement = connexion.createStatement();
+				ResultSet result = statement.executeQuery(sql);) {
 
 			while (result.next()) {
 				int id = result.getInt(1);
@@ -55,10 +51,6 @@ public class PizzaDaoDataBase implements IDao<Pizza> {
 						id + ". " + reference + " - " + libelle + " (" + prix + " €)" + " [" + url_image + "]");
 
 			}
-
-			result.close();
-			statement.close();
-
 		} catch (SQLException e) {
 			throw new StockageException(e);
 		}
@@ -67,6 +59,29 @@ public class PizzaDaoDataBase implements IDao<Pizza> {
 
 	@Override
 	public void save(Pizza t) throws StockageException {
+
+		try (Connection connexion = DriverManager.getConnection(dbURL, user, password)) {
+
+			String sql = "INSERT INTO pizza (libelle, reference, prix, url_image) VALUES (?, ?, ?, ?)";
+
+			PreparedStatement statement = connexion.prepareStatement(sql);
+			String libelle = t.getNom();
+			String reference = t.getCode();
+			double prix = t.getPrix();
+			String url_image = null;
+
+			statement.setString(1, libelle);
+			statement.setString(2, reference);
+			statement.setDouble(3, prix);
+			statement.setString(4, url_image);
+			int rowsInserted = statement.executeUpdate();
+
+			if (rowsInserted > 0) {
+				System.out.println("A new user was inserted successfully!");
+			}
+		} catch (SQLException e) {
+			throw new StockageException(e);
+		}
 
 	}
 
